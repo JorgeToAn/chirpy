99f6d3ec96c24e9fa133f9682e4d6fd78dda8e49
JorgeToAn <jorgetosta.f@gmail.com>
2026-04-02 19:37:43 -0700

    reworked password hash tests

--- internal/auth/password_test.go
+++ internal/auth/password_test.go
@@ -2,28 +2,55 @@ package auth
 
 import "testing"
 
-func TestHashPassword(t *testing.T) {
-	passwords := [5]string{"123testing", "securepa$$w0rd", "MyPaS$", "admin", "password"}
-	hashes := []string{}
-
-	for _, password := range passwords {
-		hash, err := HashPassword(password)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if hash == password {
-			t.Error("expected hash and password to not be the same")
-		}
-		hashes = append(hashes, hash)
+func TestCheckPasswordHash(t *testing.T) {
+	validPassword := "password123"
+	validHash, err := HashPassword(validPassword)
+	if err != nil {
+		t.Error(err)
+		return
 	}
 
-	for i, hash := range hashes {
-		match, err := CheckPasswordHash(passwords[i], hash)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !match {
-			t.Error("expected match between password and hash")
-		}
+	tests := []struct {
+		name          string
+		password      string
+		hash          string
+		expectedMatch bool
+		wantErr       bool
+	}{
+		{
+			name:          "Password Match",
+			password:      validPassword,
+			hash:          validHash,
+			expectedMatch: true,
+			wantErr:       false,
+		},
+		{
+			name:          "Password No Match",
+			password:      "other-passw0rd",
+			hash:          validHash,
+			expectedMatch: false,
+			wantErr:       false,
+		},
+		{
+			name:          "Invalid Hash",
+			password:      validPassword,
+			hash:          "not-valid",
+			expectedMatch: false,
+			wantErr:       true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			match, err := CheckPasswordHash(tt.password, tt.hash)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CheckPasswordHash() err = %v; wantErr = %v", err, tt.wantErr)
+				return
+			}
+
+			if match != tt.expectedMatch {
+				t.Errorf("CheckPasswordHash() = %v; expected = %v", match, tt.expectedMatch)
+			}
+		})
 	}
 }
